295182cd36daa285dbd69a6e2f5b7bde09f88470
Govhack Team <govhack@mobile1.pandaha.de>
2012-06-02 20:10:12 +1000

    Speakers and speeches.

--- hansard-parser/main.py
+++ hansard-parser/main.py
@@ -6,6 +6,11 @@ def string_to_date(string):
 	date = string.split("-")
 	return datetime.date(int(date[0]), int(date[1]), int(date[2]))
 
+def string_to_time(string):
+	time = string.split(":")
+	return datetime.time(int(time[0]), int(time[1]), 0)
+
+
 class Hansard(object):
 	def __init__(self, filename):
 		self.filename = filename
@@ -30,18 +35,41 @@ class Hansard(object):
 		debates = tree.xpath("//debate[debateinfo/type='BILLS']")
 		for debate in debates:
 			subdebates = debate.xpath("subdebate.1")
-			print len(subdebates)
 			for subdebate in subdebates:
 				name = subdebate.find("subdebateinfo/title").text
 				bill = Bill(name=name)
 
 #	get the subdebate.2 siblings of this subdebate
 				for subdebate2 in subdebate.iterfind("subdebate.2"):
-					reading = speech.find("subdebateinfo/title").text
+					reading = subdebate2.find("subdebateinfo/title").text
 					print reading
 					for speech in subdebate2.iterfind("speech"):
+						talktext = speech.find("talk.text")
+						speech_words = ElementTree.tostring(talktext, pretty_print=True)
+						the_speech = Speech(speech_words=speech_words, reading=reading)
 #	talk.start, talk.text, continue
-#	talk.start > talker > name, name.id, electorate, party, in.gov, first.speech
+#	talk.start > talker > time.stamp, name, name.id, electorate, party, in.gov, first.speech
+						talker = speech.find("talk.start/talker")
+
+						the_speech.electorate = talker.find("electorate").text.strip()
+						the_speech.party = talker.find("party").text.strip()
+
+						# almost always empty
+						if talker.find("time.stamp").text:
+							the_speech.set_datetime(talker.find("time.stamp").text, self.date)
+						else:
+						#	inside the body text we get timestamps too
+							time = talktext.xpath(".//span[@class='HPS-Time']")
+							if len(time):
+								the_speech.set_datetime(time[0].text, self.date)
+
+						speaker = Speaker()
+						speaker.speaker_name = talker.find("name").text.strip()
+						speaker.speaker_id = talker.find("name.id").text.strip()
+						speaker.generate_hash()
+
+						the_speech.speaker_hash = speaker.speaker_hash
+						
 				
 			
 			#bill = Bill()
@@ -52,103 +80,6 @@ class Bill(object):
 		self.name = name.strip()
 		status = None
 		print "Created new Bill %s." % self.name
-
-
-def _parse_xml_file(filename):
-	tree = ElementTree.parse(filename)
-	old = False
-	if old:
-		for node in tree.getiterator():
-	#		print node.tag,
-			if node.tag == "debates":
-				node.filename = filename
-				_parse_debate(node)
-			else:
-				print node.tag
-
-	else:
-
-		for debate in tree.iter("debate"):
-			print debate
-
-
-def _parse_debate(debate):
-	for node in debate.getiterator():
-		print node.tag
-
-
-def _parse_debate_old(debate):
-	major = minor = None
-	for node in debate.getiterator():
-		if node.tag == "major-heading":
-			major = node
-			if "BILL" in major.text:
-				major.filename = debate.filename
-				major.bill = True
-			else:
-				major.bill = False
-				print "Bill"
-			#else:
-			#	print "%s %s" % (major.tag, major.text.strip())
-		elif node.tag == "minor-heading":
-			minor = node
-			if "BILL" in major.text:
-				print "%s %s" % (minor.tag, minor.text.strip())
-		elif node.tag == "speech":
-			_parse_speech(node, major, minor)
-		elif node.tag == "division":
-			_parse_division(node, major, minor)
-		else:
-		#	print node.tag,
-			pass	
-
-
-"""
-<speech id="uk.org.publicwhip/lords/2006-02-07.6.1" speakerid="uk.org.publicwhip/lord/100116" speakername="Rod Kemp" time="13:00:00" url="http://parlinfo.aph.gov.au/parlInfo/search/display/display.w3p;query=Id:chamber/hansards/2006-02-07/0000">
-"""
-def _parse_speech(speech, major=None, minor=None):
-	if major.bill:
-		for node in speech.getiterator():
-			if node.tag == "speech":
-				print "speech %s" % node.tag
-				if "speakerid" in node.attrib:
-					speaker = Speaker(speaker_id=node.attrib["speakerid"], speaker_name=node.attrib["speakername"])
-					#print node.attrib
-					print speaker
-				else:
-					speaker = Speaker()
-
-				aph_id = node.attrib['id']
-				time = node.attrib['time']
-				filename = major.filename
-				speach_date = filename[:-4]
-				speach_date = datetime.date(speach_date.split("-"))
-				d = date, time
-				print d
-				print speaker.speaker_hash
-				url = node.attrib['url']
-
-				this_speech = Speech(speaker_hash=speaker.speaker_hash, aph_id=aph_id, datetime=time, url=url)
-
-	pass
-
-
-"""
-<division divdate="2006-02-07" divnumber="1" id="uk.org.publicwhip/debate/2006-02-07.33.1" nospeaker="true" time="16:32:00" url="http://parlinfo.aph.gov.au/parlInfo/search/display/display.w3p;query=Id:chamber/hansardr/2006-02-07/0000">
-
-division divisioncount
-<divisioncount ayes="34" noes="38" tellerayes="1" tellernoes="1"/>
-
-division memberlist
-<memberlist vote="aye">
-
-division member
-<member id="uk.org.publicwhip/lord/100003" vote="aye">Lyn Fay Allison</member>
-"""
-def _parse_division(division, major=None, minor=None):
-	for node in division.getiterator():
-		# print "division %s" % node.tag
-		pass
 
 
 class Speaker(object):
@@ -180,8 +111,19 @@ class Speaker(object):
 
 
 class Speech(object):
-	def __init__(self, speaker_hash, aph_id, datetime, url):
-		pass
+	def __init__(self, speech_words=None, speaker_hash=None, reading=None):
+		self.speaker_hash = speaker_hash
+		self.speech_words = speech_words
+		self.reading = reading
+
+		self.party = None
+		self.electorate = None
+
+	def set_datetime(self, time, date):
+		time = string_to_time(time)
+		self.datetime = datetime.datetime(date.year, date.month, date.day, time.hour, time.minute, time.second)
+		print self.datetime
+
 
 
 def test():
@@ -192,4 +134,9 @@ def test():
 	# _parse_xml_file(source)
 
 
+def main():
+	pass
+
+
 test()
+
